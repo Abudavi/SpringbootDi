0edcd59e787344bab49d79fcfa7135998c433b73
David MTZD <davidmtzd21512@gmail.com>
2024-09-16 19:00:54 -0600

    Creacion del proyecto

---
+++ src/test/java/com/david/springboot/di/app/springbootdi/SpringbootdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.springboot.di.app.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
