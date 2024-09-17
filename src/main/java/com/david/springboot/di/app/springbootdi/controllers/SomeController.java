c9d5f699e2dcd9fb6c19b99483bbc868d6b0e311
David MTZD <davidmtzd21512@gmail.com>
2024-09-17 14:45:53 -0600

    se creo el controlador y mapeo para obtener todos los productos con impuestos y para buscarlos por id

---
+++ src/main/java/com/david/springboot/di/app/springbootdi/controllers/SomeController.java
@@ -0,0 +1,30 @@
+package com.david.springboot.di.app.springbootdi.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.david.springboot.di.app.springbootdi.models.Product;
+import com.david.springboot.di.app.springbootdi.services.ProductService;
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    private ProductService productService;
+
+    @GetMapping("/")
+    public List<Product> list() {
+
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+
+}
