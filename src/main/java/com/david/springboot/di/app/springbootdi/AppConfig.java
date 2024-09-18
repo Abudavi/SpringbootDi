728ac569e2b5282e6128123e43ac8b9bdb5a7e6a
David MTZD <davidmtzd21512@gmail.com>
2024-09-18 17:42:31 -0600

    se agrego bean para poder  hacer la implementacion en el repositorio

--- src/main/java/com/david/springboot/di/app/springbootdi/AppConfig.java
+++ src/main/java/com/david/springboot/di/app/springbootdi/AppConfig.java
@@ -1,10 +1,18 @@
 package com.david.springboot.di.app.springbootdi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import com.david.springboot.di.app.springbootdi.repositories.IProductRepository;
+import com.david.springboot.di.app.springbootdi.repositories.ProductRepositoryJson;
 
 @Configuration
 @PropertySource("classpath:config.properties")
 public class AppConfig {
 
+    @Bean("productJson")
+    IProductRepository productRepositoryJson() {
+        return new ProductRepositoryJson();
+    }
 }
