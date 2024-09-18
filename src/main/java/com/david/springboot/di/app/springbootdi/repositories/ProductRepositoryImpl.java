8a1702370bafef5755ee7a6f4563aaf158c546f8
David MTZD <davidmtzd21512@gmail.com>
2024-09-18 13:13:06 -0600

    se modifico el nombre de la clase

---
+++ src/main/java/com/david/springboot/di/app/springbootdi/repositories/ProductRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.david.springboot.di.app.springbootdi.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.david.springboot.di.app.springbootdi.models.Product;
+
+//se optienen solo los datos
+public class ProductRepositoryImpl implements IProductRepository {
+
+    private List<Product> data;
+
+    public ProductRepositoryImpl() {
+        this.data = Arrays.asList(
+                new Product(1L, "Memoria Coirsair", 300L),
+                new Product(2L, "Cpu i5 ", 850L),
+                new Product(3L, "Teclado razer 60%", 180L),
+                new Product(4L, "Motherboard gigabyte", 490L));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return data;
+    }
+
+    @Override
+    public Product findById(Long id) {
+
+        return data.stream().filter(p -> p.getId().equals(id)).findFirst().orElseThrow();
+    }
+
+}
