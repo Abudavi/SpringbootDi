89e2d317711e2f5982b05ace206add6fe3209444
David MTZD <davidmtzd21512@gmail.com>
2024-09-18 15:42:16 -0600

    se especifico la etiqueta de component a repository

--- src/main/java/com/david/springboot/di/app/springbootdi/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/david/springboot/di/app/springbootdi/repositories/ProductRepositoryImpl.java
@@ -3,12 +3,12 @@ package com.david.springboot.di.app.springbootdi.repositories;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.david.springboot.di.app.springbootdi.models.Product;
 
 //se optienen solo los datos
-@Component
+@Repository
 public class ProductRepositoryImpl implements IProductRepository {
 
     private List<Product> data;
